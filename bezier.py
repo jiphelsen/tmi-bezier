87b0211ce90eab6361ec71aa9856f5a1ed7dd9ae
Jip Helsen <jiphelsen@student.kuleuven.be>
2024-12-18 01:11:59 +0100

    done

--- bezier.py
+++ bezier.py
@@ -17,6 +17,7 @@ class BezierCurveInteractive(FigureCanvas):
         self.fig, self.ax = plt.subplots()
         super().__init__(self.fig)
         self.grid_size = 10
+        self.selected_points = [] 
         self.n_points = n_points
         self.control_points = self.initialize_grid_points(self.n_points)
         self.dragging_point = None
@@ -41,11 +42,13 @@ class BezierCurveInteractive(FigureCanvas):
         self.fig.canvas.mpl_connect("pick_event", self.on_pick)
         self.fig.canvas.mpl_connect("motion_notify_event", self.on_motion)
         self.fig.canvas.mpl_connect("button_release_event", self.on_release)
-
+        self.fig.canvas.mpl_connect('button_press_event', self.on_click)
+        
         self.ax.set_xlim(0, 10)
         self.ax.set_ylim(0, 10)
         self.ax.set_aspect("equal", adjustable="datalim")
         self.draw()
+
 
     def initialize_grid_points(self, n_points):
         grid_size = int(np.ceil(np.sqrt(n_points)))  
@@ -82,18 +85,84 @@ class BezierCurveInteractive(FigureCanvas):
 
     def on_pick(self, event):
         if isinstance(event.artist, Circle):
-            if event.mouseevent.button == 3:  
+            if event.mouseevent.dblclick:  
+                self.handle_double_click(event.artist)
+            elif event.mouseevent.button == 3:  
+                self.deselect_point(event.artist)  
                 self.remove_point(event.artist)
-            elif event.mouseevent.button == 1: 
+            elif event.mouseevent.button == 1:  
                 self.dragging_point = event.artist
+                self.deselect_point(event.artist)
+        
+
+
+    def on_click(self, event):
+        if event.inaxes is not None:
+            if not any([circle.contains(event)[0] for circle in self.control_circles]):
+                if len(self.selected_points) == 2:
+                    self.add_point_between_selected(event)
+
+
+    def add_point_between_selected(self, event):
+        click_position = [event.xdata, event.ydata]
+
+        if len(self.selected_points) == 2:
+            idx1 = self.control_circles.index(self.selected_points[0])
+            idx2 = self.control_circles.index(self.selected_points[1])
+
+            if idx1 > idx2:
+                idx1, idx2 = idx2, idx1
+
+            new_control_points = np.insert(self.control_points, idx2, [click_position], axis=0)
+
+            new_circle = Circle(click_position, 0.3, color="red", picker=True)
+            self.ax.add_patch(new_circle)
+            
+            new_control_circles = self.control_circles[:idx2] + [new_circle] + self.control_circles[idx2:]
+
+            self.control_points = new_control_points
+            self.control_circles = new_control_circles
+
+            for circle in self.control_circles:
+                circle.set_facecolor('red')
+
+            self.selected_points = []
+
+            self.control_line.set_data(self.control_points[:, 0], self.control_points[:, 1])
+
+            self.update_curve()
+
+
+
+    def deselect_point(self, circle):
+        if circle in self.selected_points:
+            print("deselecting")
+            circle.set_facecolor('red') 
+            self.selected_points.remove(circle)  
+            self.update_curve()
+
     def remove_point(self, circle):
         if len(self.control_points) > 2:  
             idx = self.control_circles.index(circle)
             self.control_points = np.delete(self.control_points, idx, axis=0)
-
-            
             self.control_circles.pop(idx).remove()
             self.update_curve()
+    def handle_double_click(self, circle):
+        if circle in self.selected_points:
+            return
+        
+        if len(self.selected_points) < 2: 
+            self.selected_points.append(circle)
+            circle.set_facecolor('yellow')
+        else:  
+            self.selected_points[0].set_facecolor('red') 
+            self.selected_points[0].set_edgecolor('red')
+            self.selected_points.pop(0) 
+            self.selected_points.append(circle) 
+            circle.set_facecolor('yellow')  
+
+        self.update_curve()
+
 
 
     def on_motion(self, event):
@@ -115,7 +184,7 @@ def main(n_points=4):
 
 
 if __name__ == "__main__":
-    POINTS = 6
+    POINTS = 2
     if len(sys.argv) > 1:
         try:
             n_points = int(sys.argv[1])
